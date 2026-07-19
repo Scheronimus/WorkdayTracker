f55b4ef64137c7f552bebc5c4a7a62f15afcc231
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2026-07-19 16:43:51 +0200

    Configure app for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/WorkdayTracker/',
   plugins: [
     react(),
     VitePWA({
@@ -14,7 +15,8 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/WorkdayTracker/',
+        scope: '/WorkdayTracker/',
       },
     }),
   ],
